14924ac42d76aae9cda18d652078c70b256eab7f
Sidak Singh Aulakh <sidakaulakh@gmail.com>
2019-04-09 12:33:28 +0530

    Step 1: Done

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,8 @@ import Header from "../components/header"
 
 export default () => (
     <div style={{ color: `teal`}}>
-        <Header />
+        <Header headerText="About Gatsby" />
+        <Header headerText="It's Good." />
         <p>Such wow. Very Reactive.</p>
     </div>
 )
